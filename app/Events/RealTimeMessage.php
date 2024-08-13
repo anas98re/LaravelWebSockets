74338578e2d62ed83059598c73c84bc626b30fe1
Anas Alrefai <101460051+anas98re@users.noreply.github.com>
2024-08-13 17:38:51 +0300

    Update RealTimeMessage.php

--- app/Events/RealTimeMessage.php
+++ app/Events/RealTimeMessage.php
@@ -1,1 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use BeyondCode\LaravelWebSockets\WebSockets\Channels\PrivateChannel;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class RealTimeMessage implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public string $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    //private Channel
+    public function broadcastOn()
+    {
+        return new PrivateChannel('events');
+    }
+
+    //public Channel
+    // public function broadcastOn(): Channel
+    // {
+    //     return new Channel('events');
+    // }
+}
 
